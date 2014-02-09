56310006595645e241519e710c47f802e016be83
brooklynb7 <brooklynb7@gmail.com>
2014-02-09 20:51:38 +0800

    remove config.js all in the repository and histories. Add config.default.js instead

---
+++ config.default.js
@@ -0,0 +1,73 @@
+//mongodb settings for appfog mongodb service START
+//如果使用 Appfog 自带的 MongoDB服务，请使用以下配置
+/*if (process.env.VCAP_SERVICES) {
+	var env = JSON.parse(process.env.VCAP_SERVICES);
+	var mongo = env['mongodb-1.8'][0]['credentials'];
+} else {
+	var mongo = {
+		"hostname": "localhost",
+		"port": 27017,
+		"username": "",
+		"password": "",
+		"name": "",
+		"db": ""
+	}
+}
+*/
+
+var mongo = {
+	"hostname": "",
+	"port": ,
+	"username": "",
+	"password": "",
+	"name": "",
+	"db": ""
+};
+
+var generate_mongo_url = function(obj) {
+	obj.hostname = (obj.hostname || 'localhost');
+	obj.port = (obj.port || 27017);
+	obj.db = (obj.db || 'test');
+	if (obj.username && obj.password) {
+		return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
+	} else {
+		return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
+	}
+};
+//mongodb settings for appfog mongodb service END
+
+var mongoskin = require('mongoskin');
+exports.db = mongoskin.db(generate_mongo_url(mongo));
+
+var i18n = require("i18n");
+i18n.configure({
+	locales: ['zh-cn', 'en'],
+	defaultLocale: 'zh-cn',
+	directory: './public/locales',
+	cookie: "i18nlocale",
+	updateFiles: false,
+	indent: "\t",
+	extension: '.js'
+});
+exports.i18n = i18n;
+
+exports.config = {
+	title: "",
+	port: process.env.PORT || 3000,
+	session_secret: process.env.SESSION_SECRET || '',
+	timeFormat1: "YYYY/MM/DD HH:mm:ss",
+	weibo: {
+		xmlns_wb: 'xmlns:wb="http://open.weibo.com/wb"',
+		app_key: ''
+	},
+	weixin: {
+		token: '',
+		msg_type:{
+			event:'event',
+			text: 'text'
+		},
+		event:{
+			subscribe:'subscribe'
+		}
+	}
+};
